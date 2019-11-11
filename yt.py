6ad9bb1da5754e8532663a3726c9f43e2b3f7c45
andythebreaker <e24074724@mail.ncku.edu.tw>
2019-11-11 21:11:39 +0800

    SHup20191111 Version = 8no msg!

--- yt.py
+++ yt.py
@@ -119,9 +119,10 @@ plt.subplots_adjust(left=0.44, bottom=None, right=0.99, top=None, wspace=None, h
 #plt.rcParams['figure.dpi'] = 300 #解析度
 #up no use
 
-plt.figure(figsize=(10, 5))
+#WRONG!!matplotlib.rc('figsize',(10,5))
+#plt.(figsize=(10, 5))
 
-plt.savefig('op.png')
+plt.savefig('op.png',dpi=300)
 
 #print(strALL)
 #normal_samples = np.random.normal(size = 100000) # 生成 100000 組標準常態分配（平均值為 0，標準差為 1 的常態分配）隨機變數
